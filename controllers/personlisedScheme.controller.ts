b78681245361e70128dd1f3d7b0f6854296bfa89
AbhishekSaini04 <abhisainiji46@gmail.com>
2026-03-29 06:22:15 +0530

    final changes

--- controllers/personlisedScheme.controller.ts
+++ controllers/personlisedScheme.controller.ts
@@ -260,7 +260,7 @@ function debounce<T extends (...args: any[]) => any>(
   let timeout: ReturnType<typeof setTimeout>;
   return function (...args: Parameters<T>) {
     clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), delay);
+    timeout = setTimeout(() => func.apply( args), delay);
   };
 }
 
